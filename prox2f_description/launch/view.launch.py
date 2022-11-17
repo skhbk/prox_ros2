fb4d30464cdf88409de996591e52ba76e34c7559
Hibiki Sakai <sakai_hibiki@stu.kanazawa-u.ac.jp>
2022-11-17 13:07:01 +0900

    Add launch file for viewing robot
    Add rviz config
    Update CmakeLists.txt
    Update dependency

---
+++ prox2f_description/launch/view.launch.py
@@ -0,0 +1,69 @@
+#  Copyright 2022 Sakai Hibiki
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from launch import LaunchDescription
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    PathJoinSubstitution,
+)
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("prox2f_description"), "urdf", "prox2f.urdf.xacro"]
+            ),
+            " ",
+            "ur_type:=ur5e",
+        ]
+    )
+
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[robot_description],
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=[
+            "-d",
+            PathJoinSubstitution(
+                [FindPackageShare("prox2f_description"), "rviz", "view.rviz"]
+            ),
+        ],
+        emulate_tty=True,
+    )
+
+    nodes = [
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        rviz_node,
+    ]
+
+    return LaunchDescription(nodes)
