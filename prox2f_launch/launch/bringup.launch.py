a36bf890b4d49c6aa490d963473c4d182c19e039
Hibiki Sakai <84374700+skhbk@users.noreply.github.com>
2023-02-09 20:17:13 +0900

    Add launch file for hardware bringup
    - Add controller parameter file

---
+++ prox2f_launch/launch/bringup.launch.py
@@ -0,0 +1,164 @@
+#  Copyright 2023 Sakai Hibiki
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.conditions import UnlessCondition
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import (
+    LaunchConfiguration,
+    Command,
+    FindExecutable,
+    PathJoinSubstitution,
+)
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    args = []
+    args.append(DeclareLaunchArgument("robot_ip", default_value="192.168.11.180"))
+    args.append(DeclareLaunchArgument("use_fake_hardware", default_value="false"))
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("prox2f_description"), "urdf", "prox2f.urdf.xacro"]
+            ),
+            " ",
+            "name:=prox2f",
+            " ",
+            "robot_ip:=",
+            LaunchConfiguration("robot_ip"),
+            " ",
+            "use_fake_hardware:=",
+            LaunchConfiguration("use_fake_hardware"),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    initial_controllers = PathJoinSubstitution(
+        [FindPackageShare("prox2f_launch"), "config", "prox2f_controllers.yaml"]
+    )
+
+    ur_control_node = Node(
+        package="ur_robot_driver",
+        executable="ur_ros2_control_node",
+        parameters=[
+            robot_description,
+            initial_controllers,
+        ],
+        output="screen",
+    )
+
+    dashboard_client_node = Node(
+        package="ur_robot_driver",
+        condition=UnlessCondition(LaunchConfiguration("use_fake_hardware")),
+        executable="dashboard_client",
+        name="dashboard_client",
+        output="screen",
+        emulate_tty=True,
+        parameters=[{"robot_ip": LaunchConfiguration("robot_ip")}],
+    )
+
+    controller_stopper_node = Node(
+        package="ur_robot_driver",
+        executable="controller_stopper_node",
+        name="controller_stopper",
+        output="screen",
+        emulate_tty=True,
+        condition=UnlessCondition(LaunchConfiguration("use_fake_hardware")),
+        parameters=[
+            {"headless_mode": False},
+            {"joint_controller_active": False},
+            {
+                "consistent_controllers": [
+                    "io_and_status_controller",
+                    "force_torque_sensor_broadcaster",
+                    "joint_state_broadcaster",
+                    "speed_scaling_state_broadcaster",
+                ]
+            },
+        ],
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "-c", "/controller_manager"],
+    )
+
+    io_and_status_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["io_and_status_controller", "-c", "/controller_manager"],
+    )
+
+    speed_scaling_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["speed_scaling_state_broadcaster", "-c", "/controller_manager"],
+    )
+
+    force_torque_sensor_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["force_torque_sensor_broadcaster", "-c", "/controller_manager"],
+    )
+
+    wrench_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["wrench_controller", "-c", "/controller_manager", "--inactive"],
+    )
+
+    # Rviz
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("prox2f_launch"), "rviz", "view.rviz"]
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        namespace="",
+        arguments=["-d", rviz_config_file],
+        emulate_tty=True,
+    )
+
+    actions = [
+        ur_control_node,
+        dashboard_client_node,
+        controller_stopper_node,
+        robot_state_publisher_node,
+        joint_state_broadcaster_spawner,
+        io_and_status_controller_spawner,
+        speed_scaling_state_broadcaster_spawner,
+        force_torque_sensor_broadcaster_spawner,
+        wrench_controller_spawner,
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner, on_exit=[rviz_node]
+            )
+        ),
+    ]
+
+    return LaunchDescription(args + actions)
