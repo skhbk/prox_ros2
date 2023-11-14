0c370e00329f043c58e20619c973b1fd612f239b
Hibiki Sakai <84374700+skhbk@users.noreply.github.com>
2023-11-14 16:13:41 +0900

    Remove attraction package

--- prox2f_launch/launch/proximity_pipeline.launch.py
+++ prox2f_launch/launch/proximity_pipeline.launch.py
@@ -128,28 +128,6 @@ def generate_launch_description():
             )
         )
 
-    virtual_wrench_node = ComposableNode(
-        package="prox2f_attraction",
-        plugin="prox::attraction::VirtualWrench",
-        remappings=[
-            ("input1/normals", [output_namespaces[0], "/triangulation/normals"]),
-            ("input2/normals", [output_namespaces[1], "/triangulation/normals"]),
-        ],
-        parameters=[
-            {"wrench_frame_id": "tcp", "normal_scale": 0.05, "stiffness": 1 / 64}
-        ],
-        extra_arguments=[{"use_intra_process_comms": True}],
-    )
-    wrench_to_twist_node = ComposableNode(
-        package="prox2f_attraction",
-        plugin="prox::attraction::WrenchToTwist",
-        remappings=[
-            ("input/wrench_stamped", "virtual_wrench/wrench_stamped"),
-            ("~/twist_stamped", "twist_controller/commands"),
-        ],
-        parameters=[{"mass": 0.5, "inertia": 0.001}],
-        extra_arguments=[{"use_intra_process_comms": True}],
-    )
     grasp_pose_publisher_node = ComposableNode(
         package="prox2f_pregrasp",
         plugin="prox::pregrasp::GraspPosePublisher",
@@ -162,13 +140,11 @@ def generate_launch_description():
     )
     containers.append(
         ComposableNodeContainer(
-            name="attraction_container",
+            name="pregrasp_container",
             namespace="",
             package="rclcpp_components",
             executable="component_container",
             composable_node_descriptions=[
-                virtual_wrench_node,
-                wrench_to_twist_node,
                 grasp_pose_publisher_node,
             ],
             emulate_tty=True,
